fa29eb9658e337ac800921a5cf08808a4d3c8c49
Degisew <jegisew21@gmail.com>
2023-03-10 15:14:47 +0300

    Add values to seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(text: "Hi there!")
+Message.create(text: "Good Morning")
+Message.create(text: "Good Afternoon")
+Message.create(text: "Good Evening")
+Message.create(text: "Good Night")
